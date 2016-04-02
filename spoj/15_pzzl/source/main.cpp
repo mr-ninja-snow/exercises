a7d3ea96a7ee50a6e60fe3d4b8a3e4c402774ef0
mr-ninja-snow <mr.ninja.snow@gmail.com>
2016-04-02 13:32:49 +0300

    clean up in main.cpp; added distance calculation; refactored istream and ostream usage;

--- spoj/15_pzzl/source/main.cpp
+++ spoj/15_pzzl/source/main.cpp
@@ -12,31 +12,41 @@ public :
 		m_evenCycleSolution(false )
 	{};
 
-	friend std ::istream& operator>>( std::istream & input, FifteenPuzzle& p)
+	friend std::istream& operator>>( std::istream & input, FifteenPuzzle& p)
 	{
 		int mSqr = p.m_m * p. m_m;
 		bool startingEven = false;
 		bool currentSolutionType;
 
-		for ( unsigned z = 0; z < p .m_m; ++z ) {
+		p.m_distance = 0;
+		for ( unsigned z = 0; z < p.m_m; ++z ) {
 			currentSolutionType = startingEven;
 
-			for ( unsigned y = 0; y < p .m_m; ++y ) {
-				for ( unsigned x = 0; x < p .m_m; ++x ) {
+			for ( unsigned y = 0; y < p.m_m; ++y ) {
+				for ( unsigned x = 0; x < p.m_m; ++x ) {
 
 					int index = mSqr * z + p. m_m * y + x + 1;
 					int num;
 
 					input >> num;
-					p .m_puzzle. push_back(num );
-					std ::cout << currentSolutionType << ", " ;
+					p.m_puzzle. push_back(num );
+					std::cout << currentSolutionType << ", ";
 
 					if ( num == 0) {
-						p .m_evenCycleSolution = currentSolutionType;
-						p .m_puzzleMap. insert(std ::pair< int, int>(p .m_m * p.m_m * p. m_m, index ));
+
+						if (p.m_m * p.m_m * p. m_m != index) {
+							p.m_distance++;
+						}
+
+						p.m_evenCycleSolution = currentSolutionType;
+						p.m_puzzleMap. insert(std::pair< int, int>(p.m_m * p.m_m * p. m_m, index));
 					}
 					else {
-						p .m_puzzleMap. insert(std ::pair< int, int>(num , index));
+						if (num != index) {
+							p.m_distance++;
+						}
+
+						p.m_puzzleMap. insert(std::pair< int, int>(num , index));
 					}
 
 					currentSolutionType = ! currentSolutionType;
@@ -51,7 +61,7 @@ public :
 	int getPermutationCycle (int missplacedNum, int nextNum , int numsInCycle)
 	{
 		numsInCycle++;
-		std::cout << "nextNum - " << nextNum << "; m_puzzleMap[nextNum] - " << m_puzzleMap[ nextNum] << "\n" ;
+		//std::cout << "nextNum - " << nextNum << "; m_puzzleMap[nextNum] - " << m_puzzleMap[ nextNum] << "\n";
 		int num = m_puzzleMap[nextNum ];
 
 		if ( missplacedNum != num) {
@@ -59,7 +69,7 @@ public :
 		}
 
 		m_puzzleMap[nextNum ] = nextNum;
-		return numsInCycle ;
+		return numsInCycle;
 	}
 
 	enum res
@@ -87,29 +97,24 @@ public :
 
 	std::string canBeSolved()
 	{
-		if ( m_evenCycleSolution)
-		{
-			std::cout << "even";
-		}
-		else
-		{
-			std::cout << "odd";
+		if (m_distance == 0) {
+			return std::string( "Puzzle can be solved.\n" );
 		}
 
 		debugPrint();
 		std::vector <int> permutationCycles;
 		for ( auto& nPair : m_puzzleMap) {
-			std::cout << "nPair.first - " << nPair.first << "; nPair.second - " << nPair .second << "\n" ;
+			// std::cout << "nPair.first - " << nPair.first << "; nPair.second - " << nPair.second << "\n";
 			if ( nPair.first != nPair.second ) {
-				int numInCycle = getPermutationCycle(nPair .first, nPair.second , 0 );
-				m_puzzleMap[nPair .first] = nPair.first ;
-				std::cout << "numInCycle - " << numInCycle << "\n";
+				int numInCycle = getPermutationCycle(nPair.first, nPair.second , 0 );
+				m_puzzleMap[nPair.first] = nPair.first;
+				// std::cout << "numInCycle - " << numInCycle << "\n";
 				permutationCycles.push_back (numInCycle);
 			}
 		}
 
 		res currentSolutionType = NONE;
-		bool isEven ;
+		bool isEven;
 		for ( auto i : permutationCycles)
 		{
 			isEven = (i % 2 == 0);
@@ -149,50 +154,52 @@ public :
 
 		if ( currentSolutionType == EVEN && m_evenCycleSolution || currentSolutionType == ODD && !m_evenCycleSolution )
 		{
-			return std ::string( "Puzzle can be solved.\n" );
+			return std::string( "Puzzle can be solved.\n" );
 		}
 		else
 		{
-			return std ::string( "Puzzle is unsolvable.\n" );
+			return std::string( "Puzzle is unsolvable.\n" );
 		}
 	}
 
 private :
 	std::map <int, int> m_puzzleMap;
 	std::vector <int> m_puzzle;
-	int m_m ;
-	bool m_evenCycleSolution ;
+	int m_m;
+	int m_distance;
+	bool m_evenCycleSolution;
 };
 
 int main( int argc , char const *argv[]) {
-	std::fstream file( "slovable.txt" , std ::fstream:: in);
+	std::fstream file_out( "ans.txt" , std::fstream::out);
+	std::fstream file( "slovable.txt" , std::fstream::in);
+
 	std::string input( "1 2 2 1 3 5 6 0 4 7" );
 	std::istringstream iss( input);
-	int t ;
-	//std::cin >> t;
-	// file >> t;
-	iss >> t;
-	std::vector <std:: string> ans;
-	//for(unsigned i = 0; i < t-2; ++i) {
+
+	//std::istream& input = std::cin;
+	std::istream& input = iss;
+	// std::istream& input = file;
+
+	std::ostream& output = std::cout;
+	std::ostream& output = iss;
+
+	int t;
+	input >> t;
 
 	for ( unsigned i = 0; i < t; ++i ) {
-		int m ;
-		//std::cin >> m;
-		// file >> m;
-		iss >> m;
+		int m;
+
+		input >> m;
 		FifteenPuzzle fp (m);
-		//std::cin >> fp;
-		// file >>fp;
-		iss >> fp;
-		//ans.push_back(fp.canBeSolved());
+
+		input >> fp;
+
 		std::cout << fp.canBeSolved () << "\n";
 	}
 
-	file.close ();
+	file.close();
+	file_out.close();
 
-	for ( auto s : ans)
-	{
-		std::cout << s;
-	}
-	return 0 ;
+	return 0;
 }
