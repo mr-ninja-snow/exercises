e166da911a9dd236049e5fae511bb963ec54696d
mr-ninja-snow <mr.ninja.snow@gmail.com>
2016-04-11 19:56:51 +0300

    working algo on most data sets;

--- spoj/aggressive_cows/source/main.cpp
+++ spoj/aggressive_cows/source/main.cpp
@@ -3,11 +3,14 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
+#include <queue>
+#include <functional>
+
 
 template<typename T>
 std::ostream& operator<<(std::ostream& out, std::vector<T> vec)
 {
-	for(auto&& i : vec) {
+	for (auto&& i : vec) {
 		out << i << " ";
 	}
 	out << "\n";
@@ -20,8 +23,15 @@ int main(int argc, char const *argv[]) {
 	// std::fstream file_out( "out.txt" , std::fstream::out);
 	// std::fstream file( "in.txt" , std::fstream::in);
 
-	// std::string inputStr( "1 5 4 1 2 8 4 9" );
-	std::string inputStr( "1 7 4 2 13 15 16 55 66 99" );
+	//std::string inputStr( "1 5 3 1 2 8 4 9" );
+	//std::string inputStr("1 5 5 1 2 8 4 9");
+	//std::string inputStr("1 5 4 1 2 8 4 9");
+	//std::string inputStr("1 5 2 1 2 8 4 9");
+	//std::string inputStr("1 5 4 1 2 8 4 9");
+	//std::string inputStr("1 2 2 1 2");
+	//std::string inputStr("1 7 4 2 13 15 16 55 66 99");
+	//std::string inputStr("1 7 2 2 13 15 16 55 66 99");
+	std::string inputStr("1 7 3 2 13 15 16 55 66 99");
 	std::istringstream iss(inputStr);
 
 	//std::istream& input = std::cin;
@@ -34,7 +44,7 @@ int main(int argc, char const *argv[]) {
 	unsigned t;
 	input >> t;
 
-	for ( unsigned i = 0; i < t; ++i ) {
+	for (unsigned i = 0; i < t; ++i) {
 		unsigned numberOfStals;
 		unsigned numberOfCows;
 
@@ -44,34 +54,29 @@ int main(int argc, char const *argv[]) {
 		std::vector<int> stalPos;
 		stalPos.reserve(numberOfStals);
 
-		for(unsigned j = 0; j < numberOfStals; ++j) {
+		for (unsigned j = 0; j < numberOfStals; ++j) {
 			int posTmp;
 			input >> posTmp;
 
 			stalPos.push_back(posTmp);
 		}
 
-		std::sort(stalPos.begin(), stalPos.end());
-
-		std::priority_queue <int, std::vector<int >, std::greater< int>> stallPositionsDiffs;
-		int currentStallPos = stalPos.front();
-		for(unsigned i = 1; i < stalPos.size() - 1; ++i) {
-			int ans = stalPos[i] - currentStallPos;
-
-			stallPositionsDiffs.push(ans);
-
-			currentStallPos = stalPos[i];
+		if (numberOfCows == 2) {
+			output << stalPos.back() - stalPos.front() << "\n";
+			continue;
 		}
 
-		int lo = stallPositionsDiffs.top();
+		std::sort(stalPos.begin(), stalPos.end());
+
+		int lo = 2;
 		int hi = stalPos.back() - stalPos.front();
 
 		output << stalPos << "\n\n";
 		while (lo < hi) {
-			int mid = lo + (hi - lo) / 2;
-			output << lo << " - " << mid << " - " << hi << "\n\n";
+			int x = lo + (hi - lo) / 2;
+			output << lo << " - " << x << " - " << hi << "\n\n";
 
-			int currentCows = 0;
+			int currentCows = 1;
 			int currentDistance = 0;
 			int currentPos = stalPos.front();
 			for (int i = 1; i < stalPos.size(); ++i)
@@ -79,19 +84,11 @@ int main(int argc, char const *argv[]) {
 				int distance = stalPos[i] - currentPos;
 
 				output << "currentDistance : " << currentDistance << "\ncurrentCows - " << currentCows << "\n";
-				output << "stalPos[i] -  currentPos =  " << stalPos[i] <<  "-" << currentPos << " = " << distance << "\n";
-				if (currentDistance + distance >= mid)
+				output << "stalPos[i] -  currentPos =  " << stalPos[i] << "-" << currentPos << " = " << distance << "\n";
+				if (currentDistance + distance >= x)
 				{
-					//int numCows = distance / mid + 1;
-					int numCows = distance / mid;
-					output << "number of cows in dist - " << numCows << "\n";
-					currentCows += numCows ? numCows : 1;
-					
-					// mns: check mod 
-					//currentCows += numCows;
-
-					//currentDistance = distance - numCows * mid;
-					currentDistance = distance - numCows * mid - (mid - currentDistance);
+					currentCows++;
+					currentDistance = distance;
 				}
 				else
 				{
@@ -102,13 +99,15 @@ int main(int argc, char const *argv[]) {
 				output << "\n\n";
 			}
 
-			if (currentCows <= (numberOfCows - 1))
+			output << "currentCows - " << currentCows << "\n\n\n";
+			//if (currentCows <= (numberOfCows - 1))
+			if (currentCows <= numberOfCows)
 			{
-				hi = mid;
+				hi = x;
 			}
 			else
 			{
-				lo = mid + 1;
+				lo = x + 1;
 			}
 		}
 		output << lo - 1 << "\n";
