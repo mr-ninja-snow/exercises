40df28826c5b41ff2ac85ee85201b175bf0be299
mr-ninja-snow <mr.ninja.snow@gmail.com>
2016-12-11 00:13:18 +0300

    implemented the merge sort find invertion algorithm;

--- spoj/inversion_count/source/main.cpp
+++ spoj/inversion_count/source/main.cpp
@@ -18,21 +18,7 @@ enum InputState
 	FIND_ANS
 };
 
-void brute_force(std::vector<int>& v, int& curAns)
-{
-	int currentArrayIndex = 0;
-	for(auto& el : v) {
-		//std::cout << tc.first << " in pos " << tc.second << "\n";
-		for(unsigned i = currentArrayIndex; i < v.size(); ++i) {
-			if(el > v[i]) {
-				curAns++;
-			}
-		}
-		currentArrayIndex++;
-	}
-}
-
-void bit_mask(std::vector<int>& v, int& curAns)
+void brute_force(std::vector<long long int>& v, long long int& curAns)
 {
 	int currentArrayIndex = 0;
 	for(auto& el : v) {
@@ -50,8 +36,11 @@ class MergeSort {
 private:
 	std::vector<long long int> v1;
 	std::vector<long long int> v2;
+	std::vector<long long int>& vRes;
 
 	int m_debugMergeCount;
+
+	long long int m_inversionCount;
 
 	enum AuxArray
 	{
@@ -61,12 +50,11 @@ private:
 
 	AuxArray m_currentAuxArray;
 public:
-	MergeSort(std::vector<long long int>&& v) : v1{v}, m_debugMergeCount{0}, m_currentAuxArray{USE_V2_TO_MERGE} { v2.resize(v1.size()); }
+	MergeSort(std::vector<long long int>&& v) : v1{v}, vRes{v1}, m_debugMergeCount{0}, m_inversionCount{0}, m_currentAuxArray{USE_V2_TO_MERGE} { v2.resize(v1.size()); }
 
 	void debugVectorPrint(std::vector<long long int>& v, std::string msg) const 
 	{
-		//debug print 
-		std::cout << msg << "\nindex|";
+		// std::cout << "debug:" << msg << "\nindex|";
 		for(unsigned i = 0; i < v.size(); ++i) {
 			std::cout << std::setw(2) << i << " ";
 		}
@@ -78,12 +66,23 @@ public:
 
 	}
 
+	long long int getInversionCount() const { return m_inversionCount; }
+
 	void sort()
 	{
-		debugVectorPrint(v1, "before sort");
+		// debugVectorPrint(v1, "before sort");
 		AuxArray resltingArray = divide_and_conquer(0, v1.size() - 1, USE_V2_TO_MERGE);
 		// divide_and_conquer(0, v1.size(),USE_V2_TO_MERGE );
-		debugVectorPrint(getMainArray(resltingArray), "after sort");
+		// debugVectorPrint(getMainArray(resltingArray), "after sort");
+		vRes = getMainArray(resltingArray);
+		std::cout << "m_inversionCount: " << m_inversionCount << "\n\n";
+	}
+
+
+
+	const std::vector<long long int>& getSortedArray()
+	{
+		return vRes;
 	}
 
 private:
@@ -92,14 +91,14 @@ private:
 	{
 		int mid = lo + (hi - lo)/2;
 
-		std::cout << "divide_and_conquer\n";
-		std::cout << "\tlo: " << lo << "\n";
-		std::cout << "\tmid: " << mid << "\n";
-		std::cout << "\thi: " << hi << "\n";
+		// std::cout << "debug:" << "divide_and_conquer\n";
+		// std::cout << "debug:" << "\tlo: " << lo << "\n";
+		// std::cout << "debug:" << "\tmid: " << mid << "\n";
+		// std::cout << "debug:" << "\thi: " << hi << "\n";
 		
 		if(hi - lo < 1) {
 			// m_currentAuxArray = toggelAuxArray(m_currentAuxArray);
-			std::cout << "return!\n";
+			// std::cout << "debug:" << "return!\n";
 			std::vector<long long int>& mainArray = getMainArray(toUse);
 			std::vector<long long int>& auxArray = getAuxArray(toUse);
 			auxArray[hi] = mainArray[hi];
@@ -108,15 +107,21 @@ private:
 
 
 
-		std::cout << "leftHalf \n";
+		// std::cout << "debug:" << "leftHalf \n";
 		auto resltingAuxLeft = divide_and_conquer(lo, mid, toUse);
 		// divide_and_conquer(lo, mid, toggelAuxArray(toUse));
 		
-		std::cout << "rightHalf \n";
+		// std::cout << "debug:" << "rightHalf \n";
 		// divide_and_conquer(mid, hi, toggelAuxArray(toUse));
 		auto resltingAuxRight = divide_and_conquer(mid + 1, hi, toUse);
 
-		//
+		if(resltingAuxLeft != resltingAuxRight) {
+			std::vector<long long int>& mainRArrayy = getMainArray(resltingAuxRight);
+			std::vector<long long int>& mainLArrayy = getMainArray(resltingAuxLeft);
+			for(unsigned i = mid + 1; i <= hi; ++i) {
+				mainLArrayy[i] = mainRArrayy[i];
+			}
+		}
 
 		merge(lo, mid + 1, hi, resltingAuxLeft);
 		return toggelAuxArray(resltingAuxLeft);
@@ -150,10 +155,10 @@ private:
 	{
 		if(toUse == USE_V2_TO_MERGE) {
 		// if(m_currentAuxArray == USE_V2_TO_MERGE) {
-			std::cout << "main array is v1\n";
+			// std::cout << "debug:" << "main array is v1\n";
 			return v1;
 		} else {
-			std::cout << "main array is v2\n";
+			// std::cout << "debug:" << "main array is v2\n";
 
 			return v2;
 		}
@@ -162,9 +167,10 @@ private:
 	// void merge(int lo, int mid, int hi)
 	void merge(int lo, int mid, int hi, AuxArray toUse)
 	{
-		std::cout << m_debugMergeCount++ << "# merging " << lo << ".."<< mid - 1 << " and " << mid << ".." << hi << "\n";
+		// std::cout << "debug:" << m_debugMergeCount++ << "# merging " << lo << ".."<< mid - 1 << " and " << mid << ".." << hi << "\n";
 		std::vector<long long int>& mainArrayy = getMainArray(toUse);
 		std::vector<long long int>& auxArray = getAuxArray(toUse);
+		// debugVectorPrint(mainArrayy, "\n\nmain array before merge\n");
 
 		unsigned int leftHalfIndex = lo;
 		unsigned int rightHalfIndex = mid;
@@ -172,61 +178,55 @@ private:
 		for(; auxIndex <= hi; ++auxIndex) {
 
 			if(leftHalfIndex >= mid) {
-				std::cout << "\t\tcpRight\n";
+				// std::cout << "debug:" << "\t\tcpRight\n";
 				while(rightHalfIndex <= hi) {
 					// auxIndex++;
 					auxArray[auxIndex] = mainArrayy[rightHalfIndex];
-					std::cout << "\t\tmainArrayy[rightHalfIndex]: " << mainArrayy[rightHalfIndex] << "<= is used\n";
+					// std::cout << "debug:" << "\t\tmainArrayy[rightHalfIndex:" << rightHalfIndex << "]: " << mainArrayy[rightHalfIndex] << "<= is used\n";
 					rightHalfIndex++;
 					auxIndex++;
 				}
 				break;
 			}
 			if(rightHalfIndex > hi) {
-				std::cout << "\t\tcp Left\n";
+				// std::cout << "debug:" << "\t\tcp Left\n";
 
 				while(leftHalfIndex < mid) {
 					// auxIndex++;
 					auxArray[auxIndex] = mainArrayy[leftHalfIndex];
-					std::cout << "\t\tmainArrayy[leftHalfIndex]: " << mainArrayy[leftHalfIndex] << "<= is used\n";
+					// std::cout << "debug:" << "\t\tmainArrayy[leftHalfIndex:" << leftHalfIndex << "]: " << mainArrayy[leftHalfIndex] << "<= is used\n";
 					leftHalfIndex++;
 					auxIndex++;
 				}
 				break;
 			}
 
-			std::cout << "\t\tauxIndex: " << auxIndex << "\n";
+			// std::cout << "debug:" << "\t\tauxIndex: " << auxIndex << "\n";
 			if(mainArrayy[leftHalfIndex] > mainArrayy[rightHalfIndex]) {
-				std::cout << "\t\trightHalf item smaller\n";
-				std::cout << "\t\tmainArrayy[rightHalfIndex]: " << mainArrayy[rightHalfIndex] << "<= is used\n";
-				std::cout << "\t\tmainArrayy[leftHalfIndex]: " << mainArrayy[leftHalfIndex] << "\n";
+				// std::cout << "debug:" << "\t\trightHalf item smaller\n";
+				// std::cout << "debug:" << "\t\tmainArrayy[rightHalfIndex:" << rightHalfIndex << "]: " << mainArrayy[rightHalfIndex] << "<= is used\n";
+				// std::cout << "debug:" << "\t\tmainArrayy[leftHalfIndex:" << leftHalfIndex << "]: " << mainArrayy[leftHalfIndex] << "\n";
 				auxArray[auxIndex] = mainArrayy[rightHalfIndex];
 				rightHalfIndex++;
+				
+				int invertions = mid - leftHalfIndex;
+				// std::cout << "invertions: " << invertions << "\n";
+				m_inversionCount += invertions;
 			} else{
-				std::cout << "\t\tleftHalf item smaller\n";
-				std::cout << "\t\tmainArrayy[rightHalfIndex]: " << mainArrayy[rightHalfIndex] << "\n";
-				std::cout << "\t\tmainArrayy[leftHalfIndex]: " << mainArrayy[leftHalfIndex] << "<= is used\n";
+				// std::cout << "debug:" << "\t\tleftHalf item smaller\n";
+				// std::cout << "debug:" << "\t\tmainArrayy[rightHalfIndex:" << rightHalfIndex << "]: " << mainArrayy[rightHalfIndex] << "\n";
+				// std::cout << "debug:" << "\t\tmainArrayy[leftHalfIndex:" << leftHalfIndex << "]: " << mainArrayy[leftHalfIndex] << "<= is used\n";
 
 				auxArray[auxIndex] = mainArrayy[leftHalfIndex];
 				leftHalfIndex++;
 			}
 
-			// std::cout << "here\n";
-
 		}
 
-		debugVectorPrint(auxArray, "auxArray after merge\n");
+		// debugVectorPrint(auxArray, "auxArray after merge\n");
 
 	}
 };
-
-void merge_sort(std::vector<int>& v)
-{
-
-}
-
-
-
 
 int main(int argc, char const *argv[]) {
 	// std::fstream file_out( "out.txt" , std::fstream::out);
@@ -239,112 +239,60 @@ int main(int argc, char const *argv[]) {
 		v.push_back(a);
 	}
 
+	std::vector<long long int> v2(v);
+	std::vector<long long int> v3(v);
+
 	MergeSort ms(std::move(v));
-	ms.sort();
-	// int curAns = 0;
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		ms.sort();
+		auto finish = std::chrono::high_resolution_clock::now();
+		auto seconds = std::chrono::duration_cast<std::chrono::seconds>(finish-start);
+		// std::cout << "debug: using My Merge sort - the array was sorted in " << seconds.count() << "s\n";
+	}
 
-	// auto start = std::chrono::high_resolution_clock::now();
-	// brute_force(v, curAns);
-	// auto finish = std::chrono::high_resolution_clock::now();
+	const std::vector<long long int>& vMS = ms.getSortedArray();
 
-	// auto seconds = std::chrono::duration_cast<std::chrono::seconds>(finish-start);
-	// std::cout << seconds.count() << "s\n";
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		std::sort(v2.begin(), v2.end());
+		auto finish = std::chrono::high_resolution_clock::now();
+		auto seconds = std::chrono::duration_cast<std::chrono::seconds>(finish-start);
+		// std::cout << "debug: using the std::sort - the array was sorted in " << seconds.count() << "s\n";
+	}
 
-	// std::cout << curAns << "\n";
+	bool correctSort = true;
+	for(unsigned i = 0; i < v2.size(); ++i) {
+		if(v2[i] != vMS[i]) {
+			correctSort = false;
+			std::cout << "Failed on elem " << i <<"#\n";
+			break;
+		}
+	}
+
+	if(correctSort) {
+		std::cout << "\n\nThe array was sorted correctly\n\n";
+	} else {
+		std::cout << "\n\nThe array wasn't sored correctly!\n\n";
+	}
+
+
+	long long int curAns = 0;
+
+	auto start = std::chrono::high_resolution_clock::now();
+	brute_force(v3, curAns);
+	auto finish = std::chrono::high_resolution_clock::now();
+
+	auto seconds = std::chrono::duration_cast<std::chrono::seconds>(finish-start);
+	std::cout << "It took the brute_force implementation " << seconds.count() << "s to find the number of invertions\n";
+
+	std::cout << "number of invertions - " << curAns << "\n";
+
+	if(ms.getInversionCount() == curAns) {
+		std::cout << "\n\nThe number of invertions match!\n";
+	} else {
+		std::cout << "\n\nERROR!!!\n\nThe number of invertions DO NOT match!\n";
+	}
 
 	return 0;
 }
-
-// int main(int argc, char const *argv[]) {
-// 	// std::fstream file_out( "out.txt" , std::fstream::out);
-// 	// std::fstream file( "in.txt" , std::fstream::in);
-
-// 	// std::string inputStr("2\n\n3\n3\n1\n2\n\n5\n2\n3\n8\n6\n1\n");
-// 	std::string inputStr("2\n\n3\n3\n1\n2\n\n5\n2\n3\n6\n8\n1\n");
-// 	std::istringstream iss(inputStr);
-
-// 	//std::istream& input = std::cin;
-// 	std::istream& input = iss;
-// 	// std::istream& input = file;
-
-// 	std::ostream& output = std::cout;
-// 	// std::ostream& output = file_out;
-
-// 	int inputIndex = 0;
-// 	std::string inputNumStr;
-
-// 	std::vector<int> ans;
-
-// 	std::vector<int> TC;
-// 	std::map<int, int> arrayPosMap;
-// 	// std::vector<std::vector<int>> testcases;
-// 	int testcasesIndex = 0;
-// 	InputState is = GET_ARRAY_SIZE;
-// 	int currentArraySize = 0;
-// 	int currentArrayIndex = 0;
-
-// 	int curNum = 0;
-// 	int curAns = 0;
-
-// 	int numberOfTestCases = 0;
-// 	while (input >> inputNumStr)
-// 	{
-// 		std::cout << "input: '" << inputNumStr<< "'\n";
-// 		if(inputIndex == 0) {
-// 			numberOfTestCases = std::stoi(inputNumStr);
-// 			inputIndex++;
-// 			continue;
-// 		}
-
-// 		switch(is)
-// 		{
-// 		case GET_ARRAY_SIZE:
-// 			currentArraySize = std::stoi(inputNumStr);
-// 			//testcases.emplace_back(currentArraySize);
-// 			TC.clear();
-// 			currentArrayIndex = 0;
-// 			is = GET_ARRAY_ELEMENT;
-// 			// std::cout << "\nsize of tc array - " << testcases.size() << "\n";
-// 			arrayPosMap.clear();
-// 			break;
-// 		case GET_ARRAY_ELEMENT:
-// 			// testcases[testcasesIndex][currentArrayIndex] = std::stoi(inputNumStr);
-// 			curNum = std::stoi(inputNumStr);
-// 			TC.push_back(curNum);
-// 			arrayPosMap.insert(std::pair<int, int>(curNum, currentArrayIndex));
-// 			// std::cout << "\nsize of tc - " << testcases[testcasesIndex].size() << "\n";
-// 			currentArrayIndex++;
-// 			if(currentArrayIndex == currentArraySize) {
-// 				testcasesIndex++;
-// 				is = FIND_ANS;
-// 			}
-// 			else
-// 			{
-// 			break;
-// 			}
-// 		case FIND_ANS:
-// 			std::cout << "\n\nhere is the map::\n";
-// 			// std::sort(TC.begin(), TC.end(), std::greater<int>());
-// 			currentArrayIndex = 0;
-// 			curAns = 0;
-// 			for(auto& el : TC) {
-// 				//std::cout << tc.first << " in pos " << tc.second << "\n";
-// 				for(unsigned i = currentArrayIndex; i < TC.size(); ++i) {
-// 					if(el > TC[i]) {
-// 						curAns++;
-// 					}
-// 				}
-
-// 				currentArrayIndex++;
-
-// 			}
-
-// 			std::cout << "current ans - " << curAns << "\n";
-// 			is = GET_ARRAY_SIZE;
-
-// 			break;
-// 		}
-// 	}
-
-// 	return 0;
-// }
